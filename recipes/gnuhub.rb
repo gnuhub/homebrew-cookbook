7794df60e9e437fd522198b9029fa95a69009c9b
gnuhub <gnuhub@gmail.com>
2014-12-12 08:52:31 +0800

    taps

---
+++ recipes/gnuhub.rb
@@ -0,0 +1,9 @@
+include_recipe 'homebrew'
+
+homebrew_tap 'homebrew/dupes'
+homebrew_tap 'homebrew/versions'
+homebrew_tap 'homebrew/homebrew-php'
+homebrew_tap 'homebrew/dupes'
+homebrew_tap 'caskroom/cask'
+homebrew_tap 'homebrew/binary'
+homebrew_tap 'gnuhub/gnuhub'
